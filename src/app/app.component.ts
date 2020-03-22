87501c668aaf05b17deff9b12849da88a0f863c4
baberarjumand <baber.arjumand@gmail.com>
2020-03-22 17:12:50 +0300

    added ngx-translate sample code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { Subscription, BehaviorSubject } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'git-practice';
+export class AppComponent implements OnInit, OnDestroy {
+  title = "git-practice";
+  sampleTextString;
+  translateSubscription: Subscription;
+
+  constructor(private translateService: TranslateService) {}
+
+  ngOnInit() {
+    // translateService.setDefaultLang("fr");
+    this.translateService.setDefaultLang("en");
+    console.log(
+      "default lang set to: " + this.translateService.getDefaultLang()
+    );
+    this.translateService.use(this.translateService.getBrowserLang());
+    console.log("current lang set to: " + this.translateService.currentLang);
+
+    this.translateSubscription = this.translateService
+      .stream("appText")
+      .subscribe(val => {
+        this.sampleTextString = val;
+      });
+  }
+
+  ngOnDestroy() {
+    this.translateSubscription.unsubscribe();
+  }
+
+  switchLang() {
+    if (this.translateService.currentLang == "en") {
+      this.translateService.use("fr");
+    } else {
+      this.translateService.use("en");
+    }
+    console.log("current lang set to: " + this.translateService.currentLang);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: "en"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
